165e35f74c95d0ae2645f47c73fb833d160326d6
Alexey Shtepa <asshtepa1@mts.ru>
2022-04-17 01:27:55 +0300

    fix for 1.18

--- contrib/common.mk
+++ contrib/common.mk
@@ -1,8 +1,6 @@
 UNAME_S = $(shell uname -s)
 PROJECT_NAME = $(shell basename `git rev-parse --show-toplevel`)
 MODULE = $(shell go list -m | xargs)
-VERSION = $(shell git describe --tags)
-VER = $(shell git describe --tags | sed 's/.*v\([0-9]\{1\}.[0-9]\{1,2\}.[0-9]\{1,2\}\).*/\1/')
 
 ifeq ($(UNAME_S),Linux)
 	SHELL=/bin/bash
