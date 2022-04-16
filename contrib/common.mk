d819415a9c067fea90e3e6ff18538d76cd43e26d
Alexey Shtepa <asshtepa1@mts.ru>
2022-04-17 01:28:45 +0300

    fix for 1.18

--- contrib/common.mk
+++ contrib/common.mk
@@ -2,11 +2,6 @@ UNAME_S = $(shell uname -s)
 PROJECT_NAME = $(shell basename `git rev-parse --show-toplevel`)
 MODULE = $(shell go list -m | xargs)
 
-ifeq ($(UNAME_S),Linux)
-	SHELL=/bin/bash
-	.SHELLFLAGS = -o pipefail -c
-endif
-
 goreadme = $(shell command -v goreadme 2> /dev/null)
 gomarkdoc = $(shell command -v gomarkdoc 2> /dev/null)
 linter = $(shell command -v golangci-lint 2> /dev/null)
