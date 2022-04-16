feace91b632eeb7ecb6b78dfae757c9b4929eb6f
Alexey Shtepa <asshtepa1@mts.ru>
2022-04-17 01:35:44 +0300

    change pipline

--- contrib/common.mk
+++ contrib/common.mk
@@ -1,6 +1,13 @@
 UNAME_S = $(shell uname -s)
 PROJECT_NAME = $(shell basename `git rev-parse --show-toplevel`)
 MODULE = $(shell go list -m | xargs)
+VERSION = $(shell git describe --tags)
+VER = $(shell git describe --tags | sed 's/.*v\([0-9]\{1\}.[0-9]\{1,2\}.[0-9]\{1,2\}\).*/\1/')
+
+ifeq ($(UNAME_S),Linux)
+	SHELL=/bin/bash
+	.SHELLFLAGS = -o pipefail -c
+endif
 
 goreadme = $(shell command -v goreadme 2> /dev/null)
 gomarkdoc = $(shell command -v gomarkdoc 2> /dev/null)
